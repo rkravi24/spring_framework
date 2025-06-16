5df6b783ad6ef4d1df680ef98f248da998362803
Ravi kumar <mrkravi03@gmail.com>
2025-06-16 22:41:58 +0530

    program using annotation and java configuration

---
+++ springprogram4/src/in/springprogram/resources/SpringConfigFile.java
@@ -0,0 +1,10 @@
+package in.springprogram.resources;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("in.springprogram.beans")
+public class SpringConfigFile {
+	
+}
